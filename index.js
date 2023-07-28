6da423e3924ecf52b8d3ce32b89e503377ba640b
Corey Foshee <coreydfoshee@gmail.com>
2023-07-28 18:21:02 -0500

    Update index.js links

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render(__dirname + "/views/index.ejs");
-  console.log(__dirname + "/views/index.ejs");
+  res.render("index.ejs");
 });
 
 app.get("/contact", (req, res) => {
-  res.render(__dirname + "/views/contact.ejs");
+  res.render("contact.ejs");
 });
 
 app.listen(port, (res, req) => {
