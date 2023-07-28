8df0dd79e738d485a6b32be31deda61da91b292c
Corey Foshee <coreydfoshee@gmail.com>
2023-07-28 16:52:54 -0500

    convert to web app

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.listen(port, (res, req) => {
+  console.log(`Server is running on port ${port}`);
+});
