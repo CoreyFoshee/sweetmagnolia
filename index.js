2b7072d0ca95e22f15a6087faa78a232a229203b
Corey Foshee <coreydfoshee@gmail.com>
2023-07-28 21:42:42 -0500

    Add Path File

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render(__dirname + "/views/index.ejs");
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+  res.render(__dirname + "/views/contact.ejs");
 });
 
 app.listen(port, (res, req) => {
