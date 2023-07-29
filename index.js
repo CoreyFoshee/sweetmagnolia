5e6a111ba68a92c11739355eaf57fd9f310b12b7
Corey Foshee <coreydfoshee@gmail.com>
2023-07-28 21:49:25 -0500

    add app.set ejs

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
