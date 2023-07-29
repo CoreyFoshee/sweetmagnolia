3a9ff2c98193814265872c5c2df68138b1275a4b
Corey Foshee <coreydfoshee@gmail.com>
2023-07-28 22:29:05 -0500

    fix view engine

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.set("view engine", "ejs");
+
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.render(__dirname + "/views/index.ejs");
+  res.render(__dirname + "/views/index");
 });
 
 app.get("/contact", (req, res) => {
-  res.render(__dirname + "/views/contact.ejs");
+  res.render(__dirname + "/views/contact");
 });
 
 app.listen(port, (res, req) => {
