07284d940ad6c34f6f8a30a1e2eafeab3d754f40
Corey Foshee <coreydfoshee@gmail.com>
2023-08-01 15:57:27 -0500

    Update Header and capitalization

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/Public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.render(__dirname + "/Views/index");
+  res.render(__dirname + "/views/index");
 });
 
 app.get("/contact", (req, res) => {
-  res.render(__dirname + "/Views/contact");
+  res.render(__dirname + "/views/contact");
 });
 
 app.listen(port, (res, req) => {
